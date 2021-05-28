92f8fca9cb95c82650f0e5d0fc611c80224321b0
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2021-05-28 15:52:24 -0400

    hid component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,22 @@
   <div class="home">
     <p>This is the Home page</p>
     <br>
-      <router-link to="/planets">Planets</router-link>
-    <br>
-     <router-link to="/starships">Starships</router-link> <!-- Link the rout that match the path -->
+    <div v-show="isHome">
+        <router-link to="/planets">Planets</router-link>
+      <br>
+        <router-link to="/starships">Starships</router-link>
+        <!-- Link the rout that match the path -->
+     </div>
     <router-view/>
   </div>
 </template>
 <script>
 export default {
   name: 'Home',
+  computed: {
+    isHome() {
+      return this.$route.name === 'Home';
+    },
+  },
 };
 </script>
